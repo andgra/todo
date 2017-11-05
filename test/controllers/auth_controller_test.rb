a68dc67911bedbd89024335dfac034691fef7156
andgrand <grddron@gmail.com>
2017-11-06 02:53:04 +0300

    авторизация

---
+++ test/controllers/auth_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class AuthControllerTest < ActionDispatch::IntegrationTest
+  test "should get login" do
+    get auth_login_url
+    assert_response :success
+  end
+
+  test "should get logout" do
+    get auth_logout_url
+    assert_response :success
+  end
+
+  test "should get reg" do
+    get auth_reg_url
+    assert_response :success
+  end
+
+end
