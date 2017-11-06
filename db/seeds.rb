492beccd32a8e9f196e75630c6bcb7340450306e
andgrand <grddron@gmail.com>
2017-11-06 03:03:10 +0300

    авторизация

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 User.create!([
-  {login: "andgra", password: "fsda", admin: true, last_visit: "2017-11-06 00:16:50"}
+  {email: "dronchik1995@yandex.ru", encrypted_password: "$2a$11$k48vWkDGTc0iGvDp7mcSb.cRkQmhDKEAxUmeF8P5EFAk4Yn8X5UkS", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2017-11-05 23:16:06", last_sign_in_at: "2017-11-05 23:16:06", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"}
 ])
