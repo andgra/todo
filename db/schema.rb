8d5ecaee669ffb1c6a919c6f176cc67c5d8b93e9
andgrand <grddron@gmail.com>
2017-11-06 03:02:13 +0300

    авторизация

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 20171105221156) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
